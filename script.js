895a70c42846c4babe1dfda60360ce4d20be1d0f
solphilos <trichosirius@gmail.com>
2023-07-05 19:24:24 -0400

    added click event for buttons

---
+++ script.js
@@ -0,0 +1,8 @@
+goToChores = () => {
+    // this will later reveal new page with chores list, hide page with buttons
+} 
+
+goToPoints = () => {
+    // this will later reveal new page with points system, hide button page
+}
+
