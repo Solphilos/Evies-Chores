3a25a07a23d4ee6d97bafd931c7363391cbb2573
solphilos <trichosirius@gmail.com>
2023-07-05 19:50:13 -0400

    added function/click-event to display/hide new pages

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
+const choresPage = document.querySelector('.choresPage');
+const pointsPage = document.querySelector('.pointsPage');
+const buttonBox = document.querySelector('.buttonBox');
+choresPage.style.display = "none";
+pointsPage.style.display = "none";
+
+
+// click function reveals "chores" page, hides all others
 goToChores = () => {
-    // this will later reveal new page with chores list, hide page with buttons
+    choresPage.style.display = "flex";
+    buttonBox.style.display = "none";
+    
 } 
 
+// click function reveals "point system" page, hides all others
 goToPoints = () => {
-    // this will later reveal new page with points system, hide button page
+    pointsPage.style.display = "flex";
+    buttonBox.style.display = "none";
 }
 
+
+   
